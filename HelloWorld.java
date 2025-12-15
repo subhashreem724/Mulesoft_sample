eb1f5ef333ddc7f89cbfec3903e7fb16683d2a6d
subhashreem724 <subhashree.mishra@nttdata.com>
2025-12-15 09:53:30 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * The HelloWorld class implements an application that
+ * simply prints "Hello, World!" to standard output.
+ */
+public class HelloWorld {
+
+    /**
+     * This is the main entry point for the Java program.
+     * @param args Unused.
+     */
+    public static void main(String[] args) {
+        // Prints "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
